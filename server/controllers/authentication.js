8ce59e0e210cf93f34b59263ae9f446716908209
Santiago Iraola <iraola.santi@gmail.com>
2018-03-10 18:26:05 -0300

    hashing passwords before saving to db

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -3,6 +3,10 @@ const User = require('../model/User');
 module.exports.signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+  if(!email || !password) {
+    return res.status(422).send({err: 'Must provide email or password'});
+  }
+
   User.findOne({ email: email }, (err, existingUser) => {
     if(err) return err;
 
--- server/model/User.js
+++ server/model/User.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
 });
+
+userSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) return next(err);
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if(err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+
+})
 
 module.exports = mongoose.model('user', userSchema);
