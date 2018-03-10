1bf4229d0918133ec82dc4791d771c129ad32fef
Santiago Iraola <iraola.santi@gmail.com>
2018-03-10 13:27:05 -0300

    user actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,16 @@
-import { CHANGE_AUTH } from './types';
-
+import { CHANGE_AUTH, FETCH_USERS } from './types';
+import axios from 'axios';
 export function authenticate(isLoggedIn) {
   return {
     type: CHANGE_AUTH,
     payload: isLoggedIn
+  }
+}
+
+export function fetchUsers() {
+  const request = axios.get('https://jsonplaceholder.typicode.com/users');
+  return {
+    type: FETCH_USERS,
+    payload: request
   }
 }
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,3 @@
 export const CHANGE_AUTH = 'CHANGE_AUTH';
+
+export const FETCH_USERS = 'FETCH_USERS';
