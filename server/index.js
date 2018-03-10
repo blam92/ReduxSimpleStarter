e01b12005d95d0c36a2ce539feaca9fa29e0f54f
Santiago Iraola <iraola.santi@gmail.com>
2018-03-10 14:53:57 -0300

    setup GET route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const http = require('http');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 3090;
+const router = require('./router');
 
 app.use(morgan('combined'));
-app.use(bodyParser.json({type: '*/*'}));
-
+app.use(express.json());
+app.use('/', router);
 app.listen(port, () => console.log('Server listening on', port));
