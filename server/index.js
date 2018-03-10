ef22597a7d6f0dd171d01ed8eb73c645a4c14e0a
Santiago Iraola <iraola.santi@gmail.com>
2018-03-10 14:45:22 -0300

    express setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const port = process.env.PORT || 3090;
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+
+app.listen(port, () => console.log('Server listening on', port));
